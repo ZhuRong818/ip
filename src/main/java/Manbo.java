eaa3d011c9874ca8cef1ac4a33721422b25b087d
ZhuRong818 <e1354499@u.nus.edu>
2025-08-14 00:09:58 +0800

    Implement task storage and listing Level-2

--- src/main/java/Manbo.java
+++ src/main/java/Manbo.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 public class Manbo {
     public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        String[] tasks= new String[100];
+        int taskCount= 0;
         String logo = " __  __    _    _   _ ____   ___   \n"
                 + "|  \\/  |  / \\  | \\ | | __ ) / _ \\  \n"
                 + "| |\\/| | / _ \\ |  \\| |  _ \\| | | | \n"
                 + "| |  | |/ ___ \\| |\\  | |_) | |_| | \n"
                 + "|_|  |_/_/   \\_\\_| \\_|____/ \\___/  \n";
-        Scanner in = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Manbo");
         System.out.println(" What can I do for you?");
@@ -20,14 +22,21 @@ public class Manbo {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
-                System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
-                System.out.println("____________________________________________________________");
+            } else if (input.equals("list")) {
+            System.out.println("____________________________________________________________");
+            for (int i= 0; i<taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
             }
+            System.out.println("____________________________________________________________");
+        } else {
+            tasks[taskCount] = input;
+            taskCount++;
+            System.out.println("____________________________________________________________");
+            System.out.println(" added: " + input);
+            System.out.println("____________________________________________________________");
         }
+    }
 
         in.close();
-
     }
 }
