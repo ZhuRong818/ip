95c4eb3803a618dee0d482545434e61f67af8a9b
ZhuRong818 <e1354499@u.nus.edu>
2025-08-13 23:59:34 +0800

    Level-0: Rename Duke to Manbo and implement skeleton

---
+++ src/main/java/Manbo.java
@@ -0,0 +1,18 @@
+public class Manbo {
+    public static void main(String[] args) {
+        String logo = " __  __    _    _   _ ____   ___   \n"
+                + "|  \\/  |  / \\  | \\ | | __ ) / _ \\  \n"
+                + "| |\\/| | / _ \\ |  \\| |  _ \\| | | | \n"
+                + "| |  | |/ ___ \\| |\\  | |_) | |_| | \n"
+                + "|_|  |_/_/   \\_\\_| \\_|____/ \\___/  \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Manbo");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+
+    }
+}
