e6b16dc32186ea9f86a16fcc323aec8413753024
ZhuRong818 <e1354499@u.nus.edu>
2025-08-14 21:02:00 +0800

    imoplement A-TextUiTesting, add AI.md

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -20,13 +20,16 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Manbo < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
+sed -i '' 's/[[:space:]]*$//' ACTUAL.TXT
+sed -i '' 's/[[:space:]]*$//' EXPECTED.TXT# GPT generated command
+
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
